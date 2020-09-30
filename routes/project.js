93361a16649ca62d00b363c2326d5d575296c2c0
cswat <connor.swatling+gh@gmail.com>
2020-09-29 23:01:18 -0600

    portfolio expansion
    Added project_types to data.json to allow stratification of different project.
    
    Added left nav links for different skillset showcases.
    
    Changes index page to present about me content.
    
    Made thumbnail image a home link.

--- routes/project.js
+++ routes/project.js
@@ -7,9 +7,10 @@ const {projects}=require("../data.json");
 router.get('/:id', (req,res)=>{
   const id=req.params.id;
   const currentProject=projects[(id)-1];
+  const project_type = projects.project_type;
   const technologies=currentProject.technologies;
   const imageURLs=currentProject.image_urls;
-  res.render('project',{currentProject, technologies, imageURLs});
+  res.render('project',{currentProject, project_type, technologies, imageURLs});
 });
 
 module.exports=router
