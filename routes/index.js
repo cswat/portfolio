8e77d2b9b0dbe655644fa0b134d43a9ede894760
cswat <connor.swatling+gh@gmail.com>
2020-09-30 16:47:55 -0600

    Portfolio update 2020.09.30
    Set up routing for left nav links.
    
    Added logic for endorsements.
    
    Added endorsements.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,62 @@
 const express=require('express');
 const router=express.Router();
-const {projects}=require("../data.json");
-
+const {data}=require("../data.json");
 
 router.get('/', (req,res)=>{
-  res.render('index', {projects});
+  res.render('index', {data});
 });
 
-router.get('/about', (req,res)=>{
-  res.render('about');
+router.get('/endorsements', (req,res)=>{
+  let endorsements = data.endorsements
+  res.render('endorsements', {data, endorsements});
 });
+
+//Setting up the following paths manually for now but future state is to build these programmatically based on data.json's project_types.
+
+router.get('/dataviz', (req,res)=>{
+  let selectedShowcase = data.project_types[0]
+
+  //create a list of projects applicable to the route and store them in showcaseProjects
+  let showcaseProjects = []
+  data.projects.forEach(project => {
+    if (project.project_type == 'dataviz') {
+      showcaseProjects.push(project)
+    };
+  })
+  res.render('showcase', {data, selectedShowcase, showcaseProjects});
+});
+
+// router.get('/graphicdesign', (req,res)=>{
+//   let selectedShowcase = data.project_types[1]
+//   let showcaseProjects = []
+//   data.projects.forEach(project => {
+//     if (project.project_type == 'graphicdesign') {
+//       showcaseProjects.push(project)
+//     };
+//   })
+//   res.render('showcase', {data, selectedShowcase, showcaseProjects});
+// });
+
+router.get('/webdev', (req,res)=>{
+  let selectedShowcase = data.project_types[1]
+  let showcaseProjects = []
+  data.projects.forEach(project => {
+    if (project.project_type == 'webdev') {
+      showcaseProjects.push(project)
+    };
+  })
+  res.render('showcase', {data, selectedShowcase, showcaseProjects});
+});
+
+// router.get('/writing', (req,res)=>{
+//   let selectedShowcase = data.project_types[3]
+//   let showcaseProjects = []
+//   data.projects.forEach(project => {
+//     if (project.project_type == 'writing') {
+//       showcaseProjects.push(project)
+//     };
+//   })
+//   res.render('showcase', {data, selectedShowcase, showcaseProjects});
+// });
 
 module.exports=router
--- routes/project.js
+++ routes/project.js
@@ -1,16 +1,15 @@
 const express=require('express');
 const router=express.Router();
-const {projects}=require("../data.json");
+const {data}=require("../data.json");
 
 
 
 router.get('/:id', (req,res)=>{
   const id=req.params.id;
-  const currentProject=projects[(id)-1];
-  const project_type = projects.project_type;
+  const currentProject=data.projects[(id)-1];
   const technologies=currentProject.technologies;
   const imageURLs=currentProject.image_urls;
-  res.render('project',{currentProject, project_type, technologies, imageURLs});
+  res.render('project',{data, currentProject, technologies, imageURLs});
 });
 
 module.exports=router
